aca30d2535778719f520fbb67092799fe43652ed
dominic <dominicrawlins98@gmail.com>
2019-02-07 00:15:32 +0000

    implemented x multiply

--- lab-2/encrypt.c
+++ lab-2/encrypt.c
@@ -7,6 +7,9 @@
 
 #include "encrypt.h"
 
+aes_gf28_t xtime( aes_gf28_t a );
+aes_gf28_t sbox( aes_gf28_t a );
+
 int main( int argc, char* argv[] ) {
   aes_gf28_t k[ 16 ] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6,
                       0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
@@ -31,5 +34,14 @@ int main( int argc, char* argv[] ) {
 
 
 aes_gf28_t xtime( aes_gf28_t a ){
-  
+  if( ( a & 0x80 ) == 0x80 ) {
+    return 0x1B ^ ( a << 1 );
+  }
+  else{
+    return ( a << 1 );
+  }
+}
+
+aes_gf28_t sbox( aes_gf28_t a ){
+
 }
