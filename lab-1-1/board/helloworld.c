dde569ab191e07cfc84df282a118d048c83ba474
Dominic Rawlins <dominicrawlins98@gmail.com>
2019-03-19 13:09:28 +0000

    read and write partially implemented- not working

--- lab-1-1/board/helloworld.c
+++ lab-1-1/board/helloworld.c
@@ -7,22 +7,42 @@
 
 #include "helloworld.h"
 
-int octetstr_rd( uint8_t* r, int n_r ){
-  if( scale_uart_rd_avail() == true){
-    for(int i = 0; i < n_r; i++){
-      r[i] = scale_uart_rd(SCALE_UART_MODE_NONBLOCKING);
-    }
-    return 1;
+uint8_t chartohex(uint8_t in){
+  if(in > 9){
+    return (in - 55);
   }
-  return 0;
+  else{
+    return(in - 48);
+  }
+}
+
+uint8_t octetstr_rd( uint8_t* r, int n_r ){
+  uint8_t hexsize[2];
+  hexsize[0] = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+  hexsize[1] = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+  scale_uart_rd(SCALE_UART_MODE_BLOCKING); //:
+  hexsize[0] = chartohex(hexsize[0]);
+  hexsize[1] = chartohex(hexsize[1]);
+  uint8_t size = (hexsize[0] * 0x10) + hexsize[1];
+  if(size > n_r){
+    size = n_r;
+  }
+  for(int i = 0; i < size; i++){
+    r[i] = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+  }
+  scale_uart_rd(SCALE_UART_MODE_BLOCKING); //CR
+  return size;
 }
 
 void octetstr_wr( const uint8_t* x, int n_x ){
   if( scale_uart_wr_avail() == true){
     for(int i = 0; i < n_x; i++){
-      scale_uart_wr(SCALE_UART_MODE_NONBLOCKING, x[n_x]);
+      scale_uart_wr(SCALE_UART_MODE_BLOCKING, x[i]);
     }
   }
+  scale_uart_wr(SCALE_UART_MODE_BLOCKING, '\x0D');//CR  for board & emulator
+  scale_uart_wr(SCALE_UART_MODE_BLOCKING, '\x0A');//LF  for emulator
+
 }
 
 int main( int argc, char* argv[] ) {
@@ -56,12 +76,16 @@ int main( int argc, char* argv[] ) {
   }
 }
 */
-    
-    uint8_t string[5] = {0,0,0,0,0};
-    int done = octetstr_rd(string, 5);
-    if(done == 1){
-      octetstr_wr(string, 5);
-    }
+
+    uint8_t* writeString = (uint8_t*)"cxauu";
+    //char done = octetstr_rd(writeString, 5);
+    octetstr_wr( writeString, 5);
+    uint8_t* readString = (uint8_t*)"        ";
+    uint8_t bytesRead = octetstr_rd(readString, 8);
+    octetstr_wr(readString, bytesRead);
+    //if(done == 1){
+      //octetstr_wr(&string, 5);
+    //}
   }
   return 0;
 }
