8f8d4a2fe2274cfe6a4756fcb294fc422c4be946
dominic <dominicrawlins98@gmail.com>
2019-03-19 17:55:44 +0000

    updated read and write

--- lab-1-1/board/helloworld.c
+++ lab-1-1/board/helloworld.c
@@ -7,38 +7,57 @@
 
 #include "helloworld.h"
 
-uint8_t chartohex(uint8_t in){
-  if(in > 9){
+uint8_t charToHex(char in){
+  if(in > 64 && in < 71){
     return (in - 55);
   }
-  else{
+  else if(in > 96 && in < 103){
+    return(in - 87);
+  }
+  else if(in > 47 && in < 58){
     return(in - 48);
   }
+  else return -1;
 }
+
+char hexToChar(uint8_t in){
+  if(in > 9){
+    return (in + 55);
+  }
+  else{
+    return(in + 48);
+  }
+}
+
 
 uint8_t octetstr_rd( uint8_t* r, int n_r ){
   uint8_t hexsize[2];
   hexsize[0] = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
   hexsize[1] = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
   scale_uart_rd(SCALE_UART_MODE_BLOCKING); //:
-  hexsize[0] = chartohex(hexsize[0]);
-  hexsize[1] = chartohex(hexsize[1]);
+  hexsize[0] = charToHex(hexsize[0]);
+  hexsize[1] = charToHex(hexsize[1]);
   uint8_t size = (hexsize[0] * 0x10) + hexsize[1];
   if(size > n_r){
     size = n_r;
   }
   for(int i = 0; i < size; i++){
-    r[i] = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+    uint8_t firstCharacter = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+    uint8_t secondCharacter = scale_uart_rd(SCALE_UART_MODE_BLOCKING);
+    r[i] = (charToHex(firstCharacter) * 0x10) + charToHex(secondCharacter);
   }
   scale_uart_rd(SCALE_UART_MODE_BLOCKING); //CR
+  scale_uart_rd(SCALE_UART_MODE_BLOCKING); //LF
   return size;
 }
 
-void octetstr_wr( const uint8_t* x, int n_x ){
-  if( scale_uart_wr_avail() == true){
-    for(int i = 0; i < n_x; i++){
-      scale_uart_wr(SCALE_UART_MODE_BLOCKING, x[i]);
-    }
+void octetstr_wr( const uint8_t* x, uint8_t n_x ){
+  scale_uart_wr( SCALE_UART_MODE_BLOCKING, hexToChar(n_x / 0x10));
+  scale_uart_wr( SCALE_UART_MODE_BLOCKING, hexToChar(n_x % 0x10));
+  scale_uart_wr(SCALE_UART_MODE_BLOCKING, ':');
+  for(int i = 0; i < n_x; i++){
+    scale_uart_wr(SCALE_UART_MODE_BLOCKING, hexToChar(x[i]/0x10));
+    scale_uart_wr(SCALE_UART_MODE_BLOCKING, hexToChar(x[i] % 0x10));
   }
   scale_uart_wr(SCALE_UART_MODE_BLOCKING, '\x0D');//CR  for board & emulator
   scale_uart_wr(SCALE_UART_MODE_BLOCKING, '\x0A');//LF  for emulator
@@ -54,19 +73,18 @@ int main( int argc, char* argv[] ) {
   //  char x[] = "hello world";
 
   while( true ) {
-    // read  the GPI     pin, and hence switch : t   <- GPI
-    bool t = scale_gpio_rd( SCALE_GPIO_PIN_GPI        );
-    // write the GPO     pin, and hence LED    : GPO <- t
-    scale_gpio_wr( SCALE_GPIO_PIN_GPO, t     );
+    // // read  the GPI     pin, and hence switch : t   <- GPI
+    // bool t = scale_gpio_rd( SCALE_GPIO_PIN_GPI        );
+    // // write the GPO     pin, and hence LED    : GPO <- t
+    // scale_gpio_wr( SCALE_GPIO_PIN_GPO, t     );
 
     // write the trigger pin, and hence LED    : TRG <- 1 (positive edge)
     scale_gpio_wr( SCALE_GPIO_PIN_TRG, true  );
     // delay for 500 ms = 1/2 s
-    scale_delay_ms( 500 );
+    // scale_delay_ms( 500 );
     // write the trigger pin, and hence LED    : TRG <- 0 (negative edge)
-    scale_gpio_wr( SCALE_GPIO_PIN_TRG, false );
     // delay for 500 ms = 1/2 s
-    scale_delay_ms( 500 );
+    // scale_delay_ms( 500 );
 
     /*  int n = strlen( x );
 
@@ -77,15 +95,17 @@ int main( int argc, char* argv[] ) {
 }
 */
 
-    uint8_t* writeString = (uint8_t*)"cxauu";
-    //char done = octetstr_rd(writeString, 5);
-    octetstr_wr( writeString, 5);
-    uint8_t* readString = (uint8_t*)"        ";
-    uint8_t bytesRead = octetstr_rd(readString, 8);
-    octetstr_wr(readString, bytesRead);
-    //if(done == 1){
-      //octetstr_wr(&string, 5);
-    //}
-  }
-  return 0;
+// uint8_t writeString[5] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA};
+// //char done = octetstr_rd(writeString, 5);
+// octetstr_wr( writeString, 0x05);
+uint8_t readString[128];
+uint8_t bytesRead = octetstr_rd(readString, 2);
+octetstr_wr(readString, bytesRead);
+//if(done == 1){
+//octetstr_wr(&string, 5);
+//}
+
+scale_gpio_wr( SCALE_GPIO_PIN_TRG, false );
+}
+return 0;
 }
